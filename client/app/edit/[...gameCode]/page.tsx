48e591a939cec5e59080eceba78a8ca15f21a3f1
Kourosh <kouroshmohajeriii8@gmail.com>
2025-12-10 13:25:08 +0100

    Edit's page error fixed

--- client/app/edit/[...gameCode]/page.tsx
+++ client/app/edit/[...gameCode]/page.tsx
@@ -185,6 +185,9 @@ export default function EditGame() {
             publishedData={publishedData}
             initialTitle={gameTitle}
             isEditMode={true}
+            gameTitle={gameTitle}
+            setGameTitle={setGameTitle}
+            onClearAll={() => {}}
           />
         </div>
       </div>
